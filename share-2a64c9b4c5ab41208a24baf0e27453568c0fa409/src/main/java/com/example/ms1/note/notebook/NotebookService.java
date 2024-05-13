badd1ade2881dcf63038dee37c0b62a05370434d
Dowon Nam <dwnam0114@gmail.com>
2024-05-13 20:32:27 +0900

    add the delete notebook

--- share-2a64c9b4c5ab41208a24baf0e27453568c0fa409/src/main/java/com/example/ms1/note/notebook/NotebookService.java
+++ share-2a64c9b4c5ab41208a24baf0e27453568c0fa409/src/main/java/com/example/ms1/note/notebook/NotebookService.java
@@ -25,4 +25,9 @@ public class NotebookService {
     public Notebook save(Notebook notebook) {
         return notebookRepository.save(notebook);
     }
+
+    public void delete(Long id){
+        notebookRepository.deleteById(id);
+    }
+
 }
